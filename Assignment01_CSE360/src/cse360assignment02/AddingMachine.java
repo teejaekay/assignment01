b84221faa8a1b7d583191b9e1e47f881731c8934
Taylor Kelly <taylorkelly27@yahoo.com>
2020-10-07 20:47:53 -0700

    initial commit

---
+++ Assignment01_CSE360/src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,39 @@
+// Taylor Kelly 
+// October 7, 2020
+// CSE 360, Th 9:00am
+
+package cse360assignment02;
+
+public class AddingMachine {
+
+	private int total; 
+	
+	public AddingMachine() {
+		
+		total = 0; 
+	}
+	
+	public int getTotal () {
+		
+		return 0;
+	}
+		  
+	public void add (int value) {
+		
+	}
+
+	public void subtract (int value) {
+		
+	}
+	
+
+	public String toString () {
+		
+		return "";
+	}
+
+	public void clear() {
+		
+	}
+		
+}
