47ec3a8c421aa7792a00d70c397723f5fb4018a1
Taylor Kelly <taylorkelly27@yahoo.com>
2020-10-07 21:15:13 -0700

    added functionality to the methods

--- Assignment01_CSE360/src/cse360assignment02/AddingMachine.java
+++ Assignment01_CSE360/src/cse360assignment02/AddingMachine.java
@@ -6,33 +6,49 @@ package cse360assignment02;
 
 public class AddingMachine {
 
+	// running total of the object
 	private int total; 
 	
+	// string that hold the history of adds/subtracts
+	private String str = "0";
+	
+	// constructor that sets total to 0
 	public AddingMachine() {
 		
 		total = 0; 
 	}
 	
+	// returns the value of total
 	public int getTotal () {
 		
-		return 0;
-	}
-		  
-	public void add (int value) {
-		
-	}
-
-	public void subtract (int value) {
-		
+		return this.total;
 	}
 	
-
-	public String toString () {
+	// adds the parameter value to the current total
+	public void add (int value) {
 		
-		return "";
+		total += value;
+		str = str + " + " + value; 
 	}
 
+	// subtracts the parameter value from the current total
+	public void subtract (int value) {
+		
+		total -= value; 
+		str = str + " - " + value;
+	}
+	
+	// returns the string that keeps track of previous adds/subtracts
+	public String toString () {
+		
+		return str;
+	}
+
+	// sets total to 0, erases history
 	public void clear() {
+		
+		this.total = 0; 
+		str = "0";
 		
 	}
 		
